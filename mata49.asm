c2d23a1e23360e7db54727fdee5a0025eebfaf5a
Matheus Marçal <71469642+zxmatheus@users.noreply.github.com>
2022-12-04 13:33:51 -0300

    Add files via upload

--- mata49.asm
+++ mata49.asm
@@ -1,8 +1,51 @@
 %include "io.inc"
 
+section .bss
+frase resb 62
+substr resb 41
+invertido resb 62
+
 section .text
 global CMAIN
 CMAIN:
+    mov ebp, esp; for correct debugging
     ;write your code here
     xor eax, eax
-    ret
+    xor ebx, ebx
+    xor ecx, ecx
+    xor esi, esi
+    xor edi, edi
+
+    GET_STRING frase, 62
+    mov esi, frase
+    add esi, 7     ;inicio da substring a ser retirada
+    mov edi, substr
+    mov ecx, 41    ;tamanho da substring a ser retirada
+    rep movsb
+    
+    PRINT_STRING substr
+    NEWLINE
+    mov edi, invertido
+    mov ecx, 62
+
+    std
+    lodsb   ;volta para o último caractere da string retirada
+inverte:
+    std     ;seta a direction flag para decrementar
+    lodsb   ;le a partir do fim da string
+    cld     ;seta a direction flag para incrementar
+    stosb   ;armazena a partir do início da string
+    dec ecx
+    jnz inverte
+    cld     ;reseta a direction flag
+    
+    PRINT_STRING invertido
+    NEWLINE
+
+    
+    
+   
+    
+
+    
+    
